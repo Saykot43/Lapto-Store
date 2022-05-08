f5965ede3fa9492cb24a7ea9a0c03b1df23e4bf8
Saykot43 <saykot.cse43@gmail.com>
2022-05-08 16:54:36 +0600

    404 page updated

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className='text-center mb-3'>
-            <img src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="" />
+        <div className='text-center mb-3 container'>
+            <img className='img-fluid' src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg" alt="" />
         </div>
     );
 };
